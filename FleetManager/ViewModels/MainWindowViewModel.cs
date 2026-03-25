757f5869fb71e3a89ab20a66a70e007df2fc12bd
Damian <damiankontakt7@gmail.com>
2026-03-25 20:17:09 +0100

    Setup MainWindowViewModel with mock vehicle collection

--- FleetManager/ViewModels/MainWindowViewModel.cs
+++ FleetManager/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace FleetManager.ViewModels;
+﻿using System.Collections.ObjectModel;
+using FleetManager.Models;
+
+namespace FleetManager.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public ObservableCollection<VehicleItemViewModel> Vehicles { get; } = new();
+
+    public MainWindowViewModel()
+    {
+        Vehicles.Add(new VehicleItemViewModel(new Vehicle { Name = "Ford Transit", RegistrationNumber = "WA 12345", FuelLevel = 80, Status = VehicleStatus.Available }));
+        Vehicles.Add(new VehicleItemViewModel(new Vehicle { Name = "Mercedes Sprinter", RegistrationNumber = "KR 98765", FuelLevel = 10, Status = VehicleStatus.InRoute }));
+        Vehicles.Add(new VehicleItemViewModel(new Vehicle { Name = "Renault Master", RegistrationNumber = "GD 45678", FuelLevel = 50, Status = VehicleStatus.Service }));
+    }
 }
