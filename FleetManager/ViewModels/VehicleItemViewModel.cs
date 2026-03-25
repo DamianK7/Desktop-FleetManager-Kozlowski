4ac3119105af09929d948f40f368ac1480c2f7ff
Damian <damiankontakt7@gmail.com>
2026-03-25 20:10:14 +0100

    Add VehicleItemViewModel

--- FleetManager/ViewModels/VehicleItemViewModel.cs
+++ FleetManager/ViewModels/VehicleItemViewModel.cs
@@ -1,7 +1,13 @@
-// (ViewModel dla UserControl)
+using FleetManager.Models;
+
 namespace FleetManager.ViewModels;
 
 public class VehicleItemViewModel : ViewModelBase
 {
-    
+    public Vehicle Vehicle { get; }
+
+    public VehicleItemViewModel(Vehicle vehicle)
+    {
+        Vehicle = vehicle;
+    }
 }
